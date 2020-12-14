b7459689b67d9dbcc0bdca575aca4c9df4b22cba
DilanMDA <akiladilan04@gmail.com>
2020-12-14 13:15:06 +0530

    first function component create

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+//  create first component 
+// start the make component normally we need to import react and javascript function
+// index.js is javascript entry point and we need to add additional code.
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// if create function we initialize this function starting the capitalize letter in this case react knows  this is the function.
+import ReactDom from 'react-dom';
+// we return to JSX and we must convert that HTML so we import ReactDom and its render JSX into HTML  
+function Greeting(){
+return <h4>This is Akila and Tis is my first component</h4>
+}
+// render method looking for two thing first one is what is going to render and second thing is where we want to render which is identify root id 
+ReactDom.render(<Greeting/>, document.getElementById('root'));
