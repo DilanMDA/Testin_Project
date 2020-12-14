24d82343ec0c1c734277d95444cec0e70b8e3887
DilanMDA <akiladilan04@gmail.com>
2020-12-14 23:41:36 +0530

    CSS Cascading Order

--- src/index.js
+++ src/index.js
@@ -31,5 +31,11 @@ const Image = () => {
 	return <img src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._AC_SX184_.jpg" alt="Amazon Book"/>
 }
 const Title = () => <h1>I Love You to the Moon and Back</h1>
-const Author = () => <h4>Amelia Hepworth, Tim Warnes</h4>
+
+// All the styles in a page will "cascade" into a new "virtual" style sheet by the following rules, where number one has the highest priority:
+// Inline style (inside an HTML element)
+// External and internal style sheets (in the head section)
+// Browser default
+// So, an inline style has the highest priority, and will override external and internal styles and browser defaults.
+const Author = () => <h4 style={{color: '#617d98',marginTop:'0.25rem'}}>Amelia Hepworth, Tim Warnes</h4>
 ReactDom.render(<BookList />, document.getElementById('root'));
