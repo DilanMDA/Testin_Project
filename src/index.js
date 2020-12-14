0ff56bbdd71e29a3ab11a413bd853a1e41e289cd
DilanMDA <akiladilan04@gmail.com>
2020-12-14 18:21:14 +0530

    Nested Component Created and checked React Tools in Chrome Browser

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-// JSX rules
-// return single Element
-// div / section / article or fragment
-// use camelCase for html attribute
-// className instead of class
-// formatting
+//  Nested Component, React Tools
 function Greeting() {
 	return (
 		<div className="">
-			<h1> use camelCase for html attribute </h1>
-			<ul>
-				<li>
-					<a href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ-NU3l8of39gqI2CDzviSADnKq26L_xFjLA&usqp=CAU">
-						hello world
-					</a>
-				</li>
-			</ul> 
+			 <Person/>
+			 <Message/>
+			 
 		</div>
 	);
+}
+
+const Person = () => <h1> Akila Dilan </h1>;
+const Message = () => {
+return <p> this is my message</p>;
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
