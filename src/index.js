6232d96771013b48da1f79cc700768fb71d3e1fc
DilanMDA <akiladilan04@gmail.com>
2020-12-14 17:59:10 +0530

    camelCase and formatting considered

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
- import React from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
- // JSX rules
-//  return single Element
-// div / section / article or fragment 
+
+// JSX rules
+// return single Element
+// div / section / article or fragment
 // use camelCase for html attribute
-// className instead of class 
+// className instead of class
 // formatting
-function Greeting(){
-return (
-    <div>
-         <h1>This is function convert to arrow function</h1>
-         <ul><li><a href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ-NU3l8of39gqI2CDzviSADnKq26L_xFjLA&usqp=CAU">hello world</a></li></ul>
-    </div>
-    <div></div>
-)
+function Greeting() {
+	return (
+		<div className="">
+			<h1> use camelCase for html attribute </h1>
+			<ul>
+				<li>
+					<a href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ-NU3l8of39gqI2CDzviSADnKq26L_xFjLA&usqp=CAU">
+						hello world
+					</a>
+				</li>
+			</ul> 
+		</div>
+	);
 }
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
