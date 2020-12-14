73e312a3f4521156c7ffc3040f4927a997235a05
DilanMDA <akiladilan04@gmail.com>
2020-12-14 16:38:39 +0530

    edtited functin into arrow function

--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ import React from 'react';
 import ReactDom from 'react-dom';
 // we return to JSX and we must convert that HTML so we import ReactDom and its render JSX into HTML  
 // Allays return JSX
-function Greeting(){
-return <h4>This is Akila and Tis is my first component</h4>
-}
+
+// function Greeting(){
+// return (
+//     <di>
+//          <h1>This is Akila and Tis is my first component</h1>
+//     </di>
+// )
+// }
+// stateless arrow functional component
+const Greeting = () =>{
+    return React.createElement(
+        'div',
+    {},
+    React.createElement('h1',{}, 'hello world'));
+} 
 // render method looking for two thing first one is what is going to render and second thing is where we want to render which is identify root id 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
