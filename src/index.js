51ba928ab11f91918a749146fe6945bb299cba17
DilanMDA <akiladilan04@gmail.com>
2020-12-16 10:35:52 +0530

    String array list display

--- src/index.js
+++ src/index.js
@@ -2,43 +2,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-// setup vars
-const firstBook = {
-	img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._AC_SX184_.jpg',
-	title: 'I Love You to the Moon and Back',
-	author: 'Amelia Hepworth, Tim Warnes',
-};
-const secondBook = {
-	img: ' https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._AC_SX184_.jpg',
-	title: 'The Very Hungry Caterpillar',
-	author: 'Eric Carle',
-}
-
+// String Array list display via wrapping a map method
+const Books  = [
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._AC_SX184_.jpg',
+		title: 'I Love You to the Moon and Back',
+		author: 'Amelia Hepworth, Tim Warnes',
+	},
+	{
+		img: ' https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._AC_SX184_.jpg',
+		title: 'The Very Hungry Caterpillar',
+		author: 'Eric Carle',
+	}
+];
+const names = ['akila','dilan', 'saman'];
+const newNames = names.map((name) =>{
+return <h1>{name}</h1>
+});
+console.log(newNames);
 function BookList() {
 	return (
 		<section className='booklists'>
-			<Book
-				img={firstBook.img}
-				title={firstBook.title}
-				author={firstBook.author}
-			>
-				<p>Props stand for "Properties." They are read-only components.
-				It is an object which stores the value of attributes of a tag and work similar to the HTML attributes.
-				It gives a way to pass data from one component to other components. It is similar to function arguments.
-				Props are passed to the component in the same way as arguments passed in a function.</p>
-				<p>Props are immutable so we cannot modify the props from inside the component. Inside the components,
-				we can add attributes called props. These attributes are available in the component as this.
-				props and can be used to render dynamic data in our render method.</p>
-				<p>When you need immutable data in the component, you have to add props to reactDom.render() method in
-				the main.js file of your ReactJS project and used it inside the component in which you need.
-				It can be explained in the below example.</p>
-			</Book>
-
-			<Book
-				img={secondBook.img}
-				title={secondBook.title}
-				author={secondBook.author} />
-
+			 {newNames}
 		</section>
 	);
 }
