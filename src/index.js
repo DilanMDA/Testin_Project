5cd28b50db5c6ed331fe4124a4e7da6fd39b3e27
DilanMDA <akiladilan04@gmail.com>
2020-12-16 21:52:24 +0530

    added key props and combine with id of object

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 // Object Array list display via wrapping a map method
-const Books  = [
+const Books = [
 	{
+		id: 1,
 		img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._AC_SX184_.jpg',
 		title: 'I Love You to the Moon and Back',
 		author: 'Amelia Hepworth, Tim Warnes',
 	},
 	{
+		id: 2,
 		img: ' https://images-na.ssl-images-amazon.com/images/I/41tyokViuNL._AC_SX184_.jpg',
 		title: 'The Very Hungry Caterpillar',
-
 		author: 'Eric Carle',
+	},
+	{
+		id: 3,
+		img: ' https://images-na.ssl-images-amazon.com/images/I/51X8sCVdbmL._AC_SX184_.jpg',
+		title: 'Stuff You Should Know',
+		author: 'Josh Clark, Chuck Bryant',
 	}
 ];
 // const names = ['akila','dilan', 'saman'];
@@ -24,15 +31,15 @@ const Books  = [
 function BookList() {
 	return (
 		<section className='booklists'>
-			 {Books.map((book) => {
+			{Books.map((book) => {
 				//  const {img, title, author} = book;
-return <Book book = {book}></Book>
-			 })}
+				return <Book key = {book.id} book={book}></Book>
+			})}
 		</section>
 	);
 }
 const Book = (props) => {
-	const { img, title, author} = props.book
+	const { img, title, author } = props.book
 	return (
 		<article className='book'>
 			<img src={img} alt="Amazon Book" />
